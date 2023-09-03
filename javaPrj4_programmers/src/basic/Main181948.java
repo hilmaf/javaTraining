6526db943db3d8d2c6175d6fc8527e0a5af9d5c5
ORLANDO\shico <glee1470@naver.com>
2023-09-03 14:49:18 +0900

    프로그래머스 코딩 기초 트레이닝

---
+++ javaPrj4_programmers/src/basic/Main181948.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Main181948 {
+
+	public static void main(String[] args) {
+		//특수문자 출력하기
+		System.out.println("!@#$%^&*(\\'\"<>?:;");
+	}
+
+}
---
+++ javaPrj4_programmers/src/basic/Main181949.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Main181949 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
