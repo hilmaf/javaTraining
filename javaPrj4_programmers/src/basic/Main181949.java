61f76d75e04b0de7236bccbe9e198e4f9abf29cd
ORLANDO\shico <glee1470@naver.com>
2023-09-03 19:04:51 +0900

    프로그래머스 코딩 기초 트레이닝
    대소문자 바꿔서 출력하기

--- javaPrj4_programmers/src/basic/Main181949.java
+++ javaPrj4_programmers/src/basic/Main181949.java
@@ -1,9 +1,22 @@
 package basic;
-
+import java.util.Scanner;
 public class Main181949 {
 
 	public static void main(String[] args) {
-		
+		  Scanner sc = new Scanner(System.in);
+	        String a = sc.next();
+	        String answer="";
+	        
+	        for(int i=0; i<a.length(); i++) {
+	            char c = a.charAt(i);
+	            if(Character.isUpperCase(c)) {
+	                answer += Character.toLowerCase(c);
+	            } else if(Character.isLowerCase(c)) {
+	                answer += Character.toUpperCase(c);
+	            }
+	        }
+	        
+	        System.out.println(answer);
 	}
 
 }
