da4b3965a40232337d1cca82fb4eb994e5747f39
ORLANDO\shico <glee1470@naver.com>
2023-09-05 23:02:57 +0900

    프로그래머스 코딩 기초 트레이닝
    문자열 곱하기

---
+++ javaPrj4_programmers/src/basic/Solution181940.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Solution181940 {
+	 public String solution(String my_string, int k) {
+	        String answer = "";
+	        for(int i=0; i<k; i++){
+	            answer += my_string;
+	        }
+	        
+	        return answer;
+	 }
+}
