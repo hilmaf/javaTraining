97d404d375765fb6ee32fd66b9e5176b7a67afe6
ORLANDO\shico <glee1470@naver.com>
2023-09-05 22:40:56 +0900

    프로그래머스 코딩 기초 트레이닝
    문자열 번갈아 출력하기

---
+++ javaPrj4_programmers/src/basic/Main.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution181942 s = new Solution181942();
+		System.out.println(s.solution("aaaaa", "bbbbb"));
+		
+	}
+
+}
