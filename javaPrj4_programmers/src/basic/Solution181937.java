7238643ac8bd4968baad4ec5abf09cf9230513d3
ORLANDO\shico <glee1470@naver.com>
2023-09-06 21:36:30 +0900

    프로그래머스 코딩 기초 트레이닝

---
+++ javaPrj4_programmers/src/basic/Solution181937.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Solution181937 {
+	public int solution(int num, int n) {
+        int answer = 0;
+        if(num%n==0) {
+            answer = 1;
+            return answer;
+        } else {
+            answer = 0;
+            return answer;
+        }
+	}
+}
