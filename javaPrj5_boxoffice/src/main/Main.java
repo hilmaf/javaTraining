9be58849f94fe22bcffb0b4eb58e2ef8c03e225b
ORLANDO\shico <glee1470@naver.com>
2023-09-07 22:25:11 +0900

    영화 예매 프로그램(Main)

---
+++ javaPrj5_boxoffice/src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.Scanner;
+
+import manager.MovieManager;
+import movie.Elemental;
+import movie.Movie;
+import movie.Oppenheimer;
+import movie.Smugglers;
+import movie.Spiderman;
+
+public class Main {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static Movie userMovie;
+	public static Movie[] movieArr = new Movie[4];
+	
+	public static void main(String[] args) {
+		
+		
+		
+//		영화 선택(영화 정보 확인 가능)
+		MovieManager movieManager = new MovieManager();
+		movieManager.selectMovie();
+
+		//영화관 선택(영화관, 시간)
+		TheaterManager theaterManager = new TheaterManager();
+		theaterManager.selectTheaterType;
+		theaterManager.selectTime;
+		
+		//좌석 선택(좌석배치도 확인 가능)
+		//좌석 예매
+		SeatManager seatManager = new SeatManager();
+		seatManager = selectSeat;
+		
+		//계산 및 출력
+
+	
+	}
+}
