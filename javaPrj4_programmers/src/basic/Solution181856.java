27528a872396713dbdde0a265b8e2ebd18a84f2e
ORLANDO\shico <glee1470@naver.com>
2023-09-19 00:44:19 +0900

    프로그래머스 Solution181856
    아 왜 안되지 내일 다시 봐야겠다

--- javaPrj4_programmers/src/basic/Solution181856.java
+++ javaPrj4_programmers/src/basic/Solution181856.java
@@ -5,6 +5,7 @@ public class Solution181856 {
 	public int solution(int[] arr1, int[] arr2) {
 		int answer = 0;
 		
+		
 		if(arr1.length > arr2.length) {
 			answer = 1;
 		} else if(arr1.length < arr2.length) {
@@ -12,9 +13,18 @@ public class Solution181856 {
 		} else if(arr1.length == arr2.length){
 			answer = 0;
 		} else {
+			
+			int sum1=0;
+			int sum2=0;
+			
 			for(int i : arr1) {
-				int sum1 += arr1[i];
+				sum1 += arr1[i];
+				sum2 += arr2[i];
 			}
+			
+			if(sum1>sum2) {answer=1;}
+			else if(sum1<sum2) {answer=-1;}
+			else { answer=0; }
 		}
 		
 		return answer;
